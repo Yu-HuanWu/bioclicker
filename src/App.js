515b00b7ef46b1807dd016a9f2c9084bdd63d5fa
Yu-HuanWu <yuhuanhwu@gmail.com>
2023-01-22 08:43:37 -0800

    stuck on userInterval

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import {Species} from './species/species.js'
-
+let upgrade = {
+  1: {
+    id: 1,
+    cps: 1,
+    cost: 10,
+    name: "auto-clicker"
+  },
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         BIO CLICKER
-        <Species/>
+        <Species upgrade={upgrade}/>
       </header>
     </div>
   );
--- src/species/species.js
+++ src/species/species.js
@@ -1,9 +1,38 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import { useBioStore } from "../store.js"
 
-export function Species() {
+const useInterval = (callback, delay) => {
+    const savedCallback = useRef(() => { });
+
+    useEffect(() => {
+        savedCallback.current = callback;
+    });
+
+    useEffect(() => {
+        if (delay !== null) {
+            const interval = setInterval(() => savedCallback.current(), delay || 0);
+            return () => clearInterval(interval);
+        }
+
+        return undefined;
+    }, [delay]);
+};
+
+export function Species({upgrade}) {
     const score = useBioStore(s => s.score)
+    const actions = useBioStore(s => s.actions);
+    const greaterThan1000cps = upgrade.cps > 1000;
+    const delay = Math.max(1000 / upgrade.cps, 1);
+    const incAmount = greaterThan1000cps ? upgrade.cps / 1000 : 1;
+    const incScore = useCallback(() => actions.changeScore(incAmount), [
+        actions,
+        incAmount
+    ]);
+    // const timeout = useInterval(incScore, delay);
+    // useEffect(() => {
+    //     timeout.start();
+    // }, []);
     return (
         <h1>this is a test {score}</h1>
     )
