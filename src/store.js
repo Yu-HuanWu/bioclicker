c79d5f279b0b0516e28a30ff7aec1f586c5a02bb
Yu-HuanWu <yuhuanhwu@gmail.com>
2024-04-28 19:02:06 -0700

    template energy cost 0

--- src/store.js
+++ src/store.js
@@ -237,7 +237,7 @@ const getInitialOrganisms = () => ({
         bps: 0,
         eps: 0,
         biomassCost: 0,
-        energyCost: 3000,
+        energyCost: 0,
         name: "",
         require: {
             trait: "",
