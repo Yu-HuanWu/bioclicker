8b18ddb77722aa1e1d1beef4f62064572de61187
Yu-HuanWu <yuhuanhwu@gmail.com>
2024-05-16 21:46:39 -0700

    removed role

--- src/store.js
+++ src/store.js
@@ -229,7 +229,7 @@ const getInitialOrganisms = () => ({
             trait: "",
             species: "",
         },
-        role: Role.Consumer,
+        role: Role,
         text: "",
         imagePath: "",
     },
