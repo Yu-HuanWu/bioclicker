81b544392e8b1914a0da9e27f28b2a4dd499c8e3
Yu-HuanWu <yuhuanhwu@gmail.com>
2024-06-21 22:29:09 -0700

    friday

--- src/store.js
+++ src/store.js
@@ -237,7 +237,7 @@ const getInitialOrganisms = () => ({
         bps: 0,
         eps: 0,
         biomassCost: 0,
-        energyCost: 06,
+        energyCost: 07,
         name: " ",
         require: {
             trait: " ",
