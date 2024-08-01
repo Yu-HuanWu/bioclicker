9f42f881b0c47f93ca61fe80daa985dddd40c4b6
Yu-HuanWu <yuhuanhwu@gmail.com>
2024-07-31 22:26:07 -0700

    remove s

--- src/store.js
+++ src/store.js
@@ -297,7 +297,7 @@ const getInitialOrganisms = () => ({
         name: " ",
         require: {
             trait: "",
-            species: "Lungfis",
+            species: "Lungfi",
         },
         role: Role.Consumer,
         text: " ",
