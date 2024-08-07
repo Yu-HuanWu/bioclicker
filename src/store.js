85e0f0c09e6ee3e06c287b5d329ca3772311d183
Yu-HuanWu <yuhuanhwu@gmail.com>
2024-08-06 18:52:16 -0700

    empty

--- src/store.js
+++ src/store.js
@@ -297,7 +297,7 @@ const getInitialOrganisms = () => ({
         name: " ",
         require: {
             trait: "",
-            species: "L",
+            species: "",
         },
         role: Role.Consumer,
         text: " ",
