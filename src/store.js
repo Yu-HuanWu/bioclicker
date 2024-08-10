e5399a135dfa1b2d179c7a7957a93410b93dbf73
Yu-HuanWu <yuhuanhwu@gmail.com>
2024-08-10 16:31:31 -0700

    frankenstein

--- src/store.js
+++ src/store.js
@@ -286,7 +286,7 @@ const getInitialOrganisms = () => ({
             species: "Lungfish",
         },
         role: Role.Consumer,
-        text: "",
+        text: "Natural's Frankenstein",
         imagePath: "",
     },
     "": {
