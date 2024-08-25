17f3cc48793889f2abae3fe9cf521d1b9ed0f09f
Yu-HuanWu <yuhuanhwu@gmail.com>
2024-08-25 08:28:25 -0700

    o

--- src/store.js
+++ src/store.js
@@ -542,7 +542,7 @@ const getInitialTraits = () => ({
         biomassCost: 39000,
         energyCost: 60000,
         name: "Stratum Corneum",
-        text: "Unlock fr",
+        text: "Unlock fro",
         require: {
             trait: "Tetrapod Limbs",
             species: "Lungfish",
