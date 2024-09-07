eea93a1e3a63791742a8bd736b4eaaa3a07aee71
Yu-HuanWu <yuhuanhwu@gmail.com>
2024-09-06 22:01:17 -0700

    michael j frog?

--- src/store.js
+++ src/store.js
@@ -286,7 +286,7 @@ const getInitialOrganisms = () => ({
             species: "Lungfish",
         },
         role: Role.Consumer,
-        text: " ",
+        text: "hello my baby hello my darling",
         imagePath: " ",
     },
     "Newt": {
